92f68e5e99dfc9634f0819beca882af4f0cd3c55
Al Jeyapal <al.jeyapal@brightsparklabs.com>
2019-07-05 09:59:42 +0930

    TERA-68: Separate commands into main_cli and make dockerfile path configurable

--- appcli/models.py
+++ appcli/models.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # # -*- coding: utf-8 -*-
 
+from pathlib import Path
 from typing import NamedTuple
 
 class Configuration(NamedTuple):
     app_name: str
+    docker_compose_file: Path
