fe87723b6d01cc1ba28e72c01945326178f4825f
Glen Wiltshire <glen.wiltshire@brightsparklabs.com>
2020-11-12 13:37:31 +1100

    APPCLI-50: Change the date format of the logger to include the timezone.

--- appcli/logger.py
+++ appcli/logger.py
@@ -20,11 +20,17 @@ import coloredlogs
 # ------------------------------------------------------------------------------
 
 LOGGER_FORMAT = "%(asctime)s %(levelname)s: %(message)s"
+LOGGER_DATE_FORMAT = "%Y-%m-%dT%H:%M:%S %Z"
 logger = logging.getLogger(__name__)
 logger.propagate = False
-coloredlogs.install(logger=logger, fmt=LOGGER_FORMAT)
+coloredlogs.install(logger=logger, fmt=LOGGER_FORMAT, datefmt=LOGGER_DATE_FORMAT)
 
 
 def enable_debug_logging():
-    coloredlogs.install(logger=logger, fmt=LOGGER_FORMAT, level=logging.DEBUG)
+    coloredlogs.install(
+        logger=logger,
+        fmt=LOGGER_FORMAT,
+        level=logging.DEBUG,
+        datefmt=LOGGER_DATE_FORMAT,
+    )
     logger.debug("Enabled debug mode")
