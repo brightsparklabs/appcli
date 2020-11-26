b3ad7ee8f178f651d169ec38a4bef76d811f261a
Thomas Anderson <thomas.anderson@brightsparklabs.com>
2020-11-26 15:00:11 +1100

    APPCLI-51: Add unit tests for string transformer

--- appcli/string_transformer.py
+++ appcli/string_transformer.py
@@ -25,7 +25,7 @@ class StringTransformer:
 
     # The supported transformation functions
     TRANSFORMATION_FUNCTIONS = {
-        STRING_TRANSFORMER_TYPE: lambda s: (s),
+        STRING_TRANSFORMER_TYPE: lambda s: (str(s)),
         "bool": lambda s: (bool(strtobool(s))),
         "int": lambda s: (int(s)),
         "float": lambda s: (float(s)),
