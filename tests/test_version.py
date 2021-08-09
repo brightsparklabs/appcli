25ba34cf1de2578841ca98cf115a277db7ed0e73
Thomas Anderson <thomas.anderson@brightsparklabs.com>
2021-08-09 21:01:40 +1000

    GH-118: Fix unit test for version command

--- tests/test_version.py
+++ tests/test_version.py
@@ -2,7 +2,7 @@
 # # -*- coding: utf-8 -*-
 
 """
-Unit tests for version command.
+Unit tests for `version` command.
 ________________________________________________________________________________
 
 Created by brightSPARK Labs
@@ -10,16 +10,36 @@ www.brightsparklabs.com
 """
 
 # vendor libraries
-import pytest
 from click.testing import CliRunner
 
 # local libraries
 from appcli.commands.version_cli import VersionCli
+from appcli.models.cli_context import CliContext
 
 
 def test_version_cli():
-    with pytest.raises(SystemExit) as exit_code:
-        runner = CliRunner()
-        result = runner.invoke(VersionCli(None).commands["version"]())
-        assert exit_code.value.code == 0
-        assert result.output == "latest\n"
+
+    # Unfortunately, there's no easy way to get access to the main cli command,
+    # so we have to test the version command directly. This requires initialising
+    # the CliContext into the desired state, including setting the `app_version`.
+    # Therefore this unit test doesn't deal with ensuring the `app_version` is
+    # correctly populated from within the create_cli function.
+    version = "1.2.3"
+    cli_context = CliContext(
+        configuration_dir=None,
+        data_dir=None,
+        additional_data_dirs=None,
+        backup_dir=None,
+        additional_env_variables=None,
+        environment="test",
+        docker_credentials_file=None,
+        subcommand_args=None,
+        debug=True,
+        app_name="APP_NAME",
+        app_version=f"{version}",
+        commands=None,
+    )
+    runner = CliRunner()
+    result = runner.invoke(VersionCli(None).commands["version"], obj=cli_context)
+    assert result.exit_code == 0
+    assert result.output == f"{version}\n"
