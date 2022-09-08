554b9d8db9772ebfdeb9b48616336b001c07a2ea
Benjamin Wiffen <benjamin.wiffen@brightsparklabs.com>
2022-09-08 15:06:22 +1000

    APPCLI-106: Add signatures for some unit tests

---
+++ tests/commands/test_install_script.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+
+"""
+Unit tests for checking the outputted install script.
+________________________________________________________________________________
+
+Created by brightSPARK Labs
+www.brightsparklabs.com
+"""
+
+# ------------------------------------------------------------------------------
+# CONSTANTS
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# TESTS
+# ------------------------------------------------------------------------------
+
+def test_install_autoconfig_default(self):
+    """ Check if autoconfig defaults to `True` when unspecified. """
+    pass
+
+def test_install_autoconfig_false(self):
+    """ Check if autoconfig can be set to `False`. """
+    pass
+
+def test_install_autoconfig_cli(self):
+    """ Check if autoconfig can be overwritten by providing 
+    `--auto-configure=false` to the cli.
+    """
+    pass
+
+# ------------------------------------------------------------------------------
+# FIXTURES
+# ------------------------------------------------------------------------------
