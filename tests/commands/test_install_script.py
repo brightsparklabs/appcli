50dc9aed3e5eb4b30c379b23ce61f9f901e225fa
Benjamin Wiffen <benjamin.wiffen@brightsparklabs.com>
2022-10-17 09:42:22 +1100

    APPCLI-106: Fill out unit tests

--- tests/commands/test_install_script.py
+++ tests/commands/test_install_script.py
@@ -9,6 +9,13 @@ Created by brightSPARK Labs
 www.brightsparklabs.com
 """
 
+# vendor libraries
+from pathlib import Path
+
+# local libraries
+from appcli.models.configuration import Configuration
+from appcli.orchestrators import DockerComposeOrchestrator
+
 # ------------------------------------------------------------------------------
 # CONSTANTS
 # ------------------------------------------------------------------------------
@@ -17,20 +24,49 @@ www.brightsparklabs.com
 # TESTS
 # ------------------------------------------------------------------------------
 
+
 def test_install_autoconfig_default(self):
-    """ Check if autoconfig defaults to `True` when unspecified. """
-    pass
+    """Check if autoconfig defaults to `True` when unspecified."""
+    assert create_configuration().auto_configure_on_install is True
+
 
 def test_install_autoconfig_false(self):
-    """ Check if autoconfig can be set to `False`. """
-    pass
+    """Check if autoconfig can be set to `False`."""
+    assert create_configuration(False).auto_configure_on_install is False
+
 
 def test_install_autoconfig_cli(self):
-    """ Check if autoconfig can be overwritten by providing 
+    """Check if autoconfig can be overwritten by providing
     `--auto-configure=false` to the cli.
     """
+    # TODO: Find an easy way to test ths.
     pass
+
 
 # ------------------------------------------------------------------------------
 # FIXTURES
 # ------------------------------------------------------------------------------
+
+def create_configuration(auto_configure_on_install: bool = None) -> Configuration:
+
+    if auto_configure_on_install is None:
+        return Configuration(
+            app_name="my-app",
+            docker_image="invalid-image-name",
+            seed_app_configuration_file=Path("resources/test_app.yml"),
+            baseline_templates_dir=Path("resources/templates/baseline"),
+            configurable_templates_dir=Path("resources/templates/configurable"),
+            orchestrator=DockerComposeOrchestrator("cli/docker-compose.yml", []),
+            stack_configuration_file=Path("resources/stack-configuration.yml"),
+        )
+    else:
+        return Configuration(
+            app_name="my-app",
+            docker_image="invalid-image-name",
+            seed_app_configuration_file=Path("resources/test_app.yml"),
+            baseline_templates_dir=Path("resources/templates/baseline"),
+            configurable_templates_dir=Path("resources/templates/configurable"),
+            orchestrator=DockerComposeOrchestrator("cli/docker-compose.yml", []),
+            stack_configuration_file=Path("resources/stack-configuration.yml"),
+            auto_configure_on_install=auto_configure_on_install
+        )
