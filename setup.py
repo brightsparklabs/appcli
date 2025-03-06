0a49cd342cc88f0117626a9a66100073eda00253
Benjamin Wiffen <benjamin.wiffen@brightsparklabs.com>
2025-03-06 16:23:38 +1100

    APPCLI-128: Update README refs to the new adoc format

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ def get_version():
 
 here = path.dirname(path.realpath(__file__))
 # get the long description from the README file
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
+with open(path.join(here, "README.adoc"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
